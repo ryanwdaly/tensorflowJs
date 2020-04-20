4cf1e389771fe48607f0f1d610d342e39e66fa03
ryan.w.daly <ryan.w.daly90@gmail.com>
2020-04-20 16:38:21 -0400

    finished lesson

--- sketch.js
+++ sketch.js
@@ -1,4 +1,46 @@
 function setup() {
     noCanvas();
-    tf.tensor([1, 2, 3, 4]).print();
+    
+
+
+    // const tense = tf.tensor3d(values, shape, "int32")
+
+    // // values are immutable, use .variable to pass tensor into a variable that can be changed
+    // const vtense = tf.variable(tense);
+
+    // tense.data().then(function(stuff) {
+    //     console.log(stuff);
+    // })
+    // tense.print();
+    // console.log(tense.dataSync());
+
+    //math using tensor
+}
+
+function draw() {
+    const values = [];
+    for (let i = 0; i < 15; i++) {
+        values[i] = random(0, 100);
+    }
+
+    tf.tidy(myStuff);
+
+    function myStuff() {
+        const shape = [5, 3];
+        const a = tf.tensor2d(values, shape, "int32");
+        const b = tf.tensor2d(values, shape, "int32"); 
+        const b_t = b.transpose();
+        
+        const c = a.matMul(b_t);
+    }
+
+    // //once something important is done, dispose of the tensors to prevent memory leak
+    // a.dispose();
+    // b.dispose();
+    // c.dispose();
+    // b_t.dispose();
+    // console.log("hello")
+
+    console.log(tf.memory().numTensors);
+    
 }
